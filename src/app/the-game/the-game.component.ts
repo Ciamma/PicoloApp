7f11e64445b5a202ec79d4ba85dfc81c05e4dc91
Daniele Ciammaruconi <30376334+Ciamma@users.noreply.github.com>
2022-12-06 16:44:50 +0100

    migliorato gestione log

--- src/app/the-game/the-game.component.ts
+++ src/app/the-game/the-game.component.ts
@@ -28,15 +28,16 @@ export class TheGameComponent implements OnInit {
   sips: Sips[]; // lista dei sorsi
   TEST: number;  /* indica il livello di cose da printare: 
                         0- nulla
-                        1- minimo(turno,frase)
-                        2- medio(turno, frase, sostituzioni, virus conclusi, virus in corso)
-                        3- massimo(turno, frase, virus conclusi, virus in corso, parole sostituite, liste e insiemi) */
+                        1- liste, set e impostazioni varie(es params)
+                        2- minimo(turno,frase)
+                        3- medio(turno, frase, sostituzioni, virus conclusi, virus in corso)
+                        4- massimo(turno, frase, virus conclusi, virus in corso, parole sostituite, liste e insiemi) */
 
 
   constructor(private navCtrl: NavController, private modalCtrl: ModalController, private platform: Platform, private toast: ToastController,
     private route: ActivatedRoute, private db: PicolodbService) {
     this.route.queryParams.subscribe(params => {
-      // console.log('params theGame: ', params);
+      this.TEST === 1 ? console.log('params theGame: ', params) : null;
       this.listaGiocatori = new ListaGiocatori(new Set(params["giocatori"]));
       this.turni = JSON.parse(params["turni"]);
       this.difficolta = params["liv"];
@@ -45,9 +46,8 @@ export class TheGameComponent implements OnInit {
     this.virus = new Listone(false);
     this.qualita = new ListaQualita();
     this.sips = [];
-    this.TEST = 1;
-    this.platform.backButton.subscribeWithPriority(100, async () => {
-    })
+    this.TEST = 0;
+    this.platform.backButton.subscribeWithPriority(100, async () => { });
   }
 
   ngOnInit() {
@@ -55,6 +55,7 @@ export class TheGameComponent implements OnInit {
     this.setSips(this.difficolta);
     this.frase = "";
     this.turnoCorrente = 0;
+    this.TEST === 1 ? console.log("turnoCorrente: ", this.turnoCorrente, ", frase: ", this.frase) : null;    
     this.newTurn();
   }
 
@@ -86,13 +87,12 @@ export class TheGameComponent implements OnInit {
       this.frasi.noMoreThanTwoPlayers();
       this.virus.noMoreThanTwoPlayers();
     }
-    this.TEST > 2 ? console.log("Da JSON - lista frasi: ", this.frasi.lista, ", lista virus: ", this.virus.lista, ", lista qualità: ", this.qualita.qualita) : null;
+    this.TEST === 1 ? console.log("Da JSON - lista frasi: ", this.frasi.lista, ", lista virus: ", this.virus.lista, ", lista qualità: ", this.qualita.qualita) : null;
   }
   onBackButtonClicked(): void {
     this.db.storeQualitaDoppie(this.qualita.qualitaUsate);
     var arrayGiocatori = Array.from(this.listaGiocatori.listaGiocatori);
-    //console.log(arrayGiocatori);
-
+    this.TEST === 1 ? console.log("array che passo ai settings: ", arrayGiocatori) : null;
     let navigationExtras: NavigationExtras = {
       queryParams: {
         giocatori: arrayGiocatori,
@@ -102,10 +102,11 @@ export class TheGameComponent implements OnInit {
     this.navCtrl.navigateForward(['config'], navigationExtras);
   }
   ionViewWillLeave() {
+    this.virus.listaVirusInCorso.clear();
     this.sips = [];
     this.turnoCorrente = 0;
     this.TurnoInizialeFinale("inizio");
-    this.virus.listaVirusInCorso.clear();
+
   }
   setSips(livello: number) {
     this.sips.length = 0;
@@ -122,7 +123,7 @@ export class TheGameComponent implements OnInit {
       this.sips.push(new Sips("sorsi", [3, 4]));
       this.sips.push(new Sips("super_sorsi", [5, 6, 7]));
     }
-    this.TEST > 2 ? console.log("lista sorsi: ", this.sips) : null;
+    this.TEST === 1 ? console.log("lista sorsi: ", this.sips) : null;
   }
 
   findWord(tipo: string, doppioni?: Set<String>): String {
@@ -131,12 +132,12 @@ export class TheGameComponent implements OnInit {
         return this.listaGiocatori.getRandomPlayer();
       default: // qualità
         let res = this.qualita.getRandomicElement(tipo);
-        this.TEST > 1 ? console.log("qualità scelta: ", res) : null;
+        [3, 4].includes(this.TEST) ? console.log("qualità scelta: ", res) : null;
         return res !== "undefined" ? res : "";
     }
   }
   substitute(frase: String, frase_f?: String): String {
-    this.TEST > 0 ? console.log("frase senza sostituzioni: ", frase) : null;
+    this.TEST > 2 ? console.log("frase senza sostituzioni: ", frase) : null;
     let giocatoriScelti = new Set<String>();
     const regex = /\{(.*?)\}/g;
     let cont: String[] = frase.match(regex);
@@ -146,21 +147,21 @@ export class TheGameComponent implements OnInit {
         delete cont[cont.indexOf(sub)];
         let subb = sub.replace("{", "").replace("}", "").trim();
         subb = subb.includes("parola") ? "parola" : subb;
-        this.TEST > 1 ? console.log("qualità da cambiare: ", subb) : null;
+        this.TEST > 2 ? console.log("qualità da cambiare: ", subb) : null;
         switch (true) {
           case subb.includes("giocatore"):
             change = this.findWord(subb, giocatoriScelti);
-            this.TEST > 1 ? console.log("giocatore scelto: ", change) : null;
+            this.TEST > 2 ? console.log("giocatore scelto: ", change) : null;
             this.listaGiocatori.addGiocatoreFrase(change);
             break;
           case subb.includes("sors"):
             change = String(this.sips.find(p => p.tipo === subb).getRandomSip());
-            this.TEST > 1 ? console.log("sorsi scelto: ", change) : null;
+            this.TEST > 2 ? console.log("sorsi scelto: ", change) : null;
             break;
           default:
             subb = raffinaCategoria(subb);
             change = this.findWord(subb);
-            this.TEST > 1 ? console.log("qualita scelta: ", change) : null;
+            this.TEST > 2 ? console.log("qualita scelta: ", change) : null;
             break;
         }
         frase = frase.replace(String(sub), String(change));
@@ -178,18 +179,18 @@ export class TheGameComponent implements OnInit {
     let turniVirus = randomTurno(this.turnoCorrente, this.turni);
     let v: Virus = new Virus(frase, frase_f);
     this.virus.setVirusInCorso(turniVirus, v);
-    this.TEST > 2 ? console.log("lista virus aggiornata: ", this.virus.listaVirusInCorso) : null;
+    [1, 4].includes(this.TEST) ? console.log("lista virus aggiornata: ", this.virus.listaVirusInCorso) : null;
   }
   chooseFrase(virus?: boolean) {
     this.TEST > 0 ? console.log("listaVirus: ", this.virus.listaVirusInCorso) : null;
     if (virus) {
       let v = this.virus.getRandomElem() as Virus;
-      this.TEST > 2 ? console.log("lista virus fatti: ", this.virus.listaFatte) : null;
+      [1, 4].includes(this.TEST) ? console.log("lista virus fatti: ", this.virus.listaFatte) : null;
       this.frase = this.substitute(v.virus, v.virus_f);
       this.tipoFrase = "virus";
     } else {
       let f = this.frasi.getRandomElem() as Frase;
-      this.TEST > 2 ? console.log("lista frasi fatte: ", this.frasi.listaFatte) : null;
+      [1, 4].includes(this.TEST) ? console.log("lista frasi fatte: ", this.frasi.listaFatte) : null;
       this.frase = this.substitute(f.frase);
       //this.frase = this.substitute("{giocatore} finisce il bicchiere {persona_finisce_bicchiere}");
       this.tipoFrase = f.getTipoFrase();
@@ -218,9 +219,9 @@ export class TheGameComponent implements OnInit {
   }
   newTurn() {
     this.TEST > 0 ? console.log("--------------------------------------------------") : null;
-    this.TEST > 0 ? console.log('turno corrente: ' + this.turnoCorrente) : null;
+    this.TEST > 1 ? console.log('turno corrente: ' + this.turnoCorrente) : null;
     this.maledizioniConcluse = this.virus.getVirusFiniti(this.turnoCorrente);
-    this.TEST > 0 ? console.log("virus conclusi in questo turno: ", this.maledizioniConcluse) : null;
+    this.TEST > 2 ? console.log("virus conclusi in questo turno: ", this.maledizioniConcluse) : null;
     this.frase.includes("Benvenuti") ? this.turnoCorrente = 1 : null; 
     switch (true) {
       case this.turnoCorrente === 0:
@@ -234,14 +235,14 @@ export class TheGameComponent implements OnInit {
         break;
       case this.turnoCorrente === this.turni:
         this.turnoCorrente += 1;
-        this.TEST > 0 ? console.log("fine partita") : null;
+        this.TEST > 1 ? console.log("fine partita") : null;
         this.TurnoInizialeFinale("fine");
         break;
       case this.turnoCorrente > this.turni:
-        this.TEST > 2 ? console.log("torno alle impostazioni") : null;
+        this.TEST >= 1 ? console.log("torno alle impostazioni") : null;
         this.db.storeQualitaDoppie(this.qualita.qualitaUsate);
         var arrayGiocatori = Array.from(this.listaGiocatori.listaGiocatori);
-        // console.log("giocatori che passo alla modale: ", this.listaGiocatori.listaGiocatori);
+        this.TEST === 1 ? console.log("giocatori che passo alla modale: ", this.listaGiocatori.listaGiocatori) : null;
         let navigationExtras: NavigationExtras = {
           queryParams: {
             giocatori: arrayGiocatori,
@@ -254,7 +255,7 @@ export class TheGameComponent implements OnInit {
   }
   async modal(mod: String) {
     let virusDaMostrare = this.virus.getVirusInCorso();
-    this.TEST > 2 ? console.log("virus modale: ", virusDaMostrare) : null;
+    this.TEST === 1 ? console.log("virus per modale: ", virusDaMostrare) : null;
     const modal = await this.modalCtrl.create({
       component: (mod === "virus" ? ModalVirusComponent : ModalPlayersComponent),
       componentProps: {
@@ -272,9 +273,10 @@ export class TheGameComponent implements OnInit {
           this.listaGiocatori.setListaGiocatori(data['data']);
           if (this.turni !== 3000 && this.turnoCorrente < this.turni) {
             const turniAggiornati = this.setNumeroTurni(this.listaGiocatori.numeroGiocatori(), this.difficolta);
-            //console.log("turni aggiornati: ", turniAggiornati, ", turno corrente: ", this.turnoCorrente);
+            this.TEST === 1 ? console.log("giocatori che passo alla modale: ", this.listaGiocatori.listaGiocatori) : null;
+            this.TEST === 1 ? console.log("turni aggiornati: ", turniAggiornati, ", turno corrente: ", this.turnoCorrente) : null;
             this.turni = turniAggiornati > this.turnoCorrente ? turniAggiornati : this.turni;
-            this.TEST > 2 ? console.log("turni aggiornati: ", this.turni, ", lista giocatori: ", this.listaGiocatori.numeroGiocatori(), 'difficolta: ', this.difficolta) : null;
+            this.TEST === 1 ? console.log("turni aggiornati: ", this.turni, ", lista giocatori: ", this.listaGiocatori.numeroGiocatori(), 'difficolta: ', this.difficolta) : null;
           }
         });
     }
